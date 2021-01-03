54e2eb8d685c4cf443c07e6fd0d5fd2715f72e6a
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-01-03 23:56:16 +0100

    Create router and add join and chat components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Chat from "./components/Chat";
+import Join from "./components/Join";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Let's build realtime chat application</h1>
-    </div>
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
   );
 }
 
