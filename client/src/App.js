bd1d53be205cf1e60f0ce3b3f5a64bdb5f2f31f7
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-01-03 23:21:56 +0100

    Initialize project

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Let's build realtime chat application</h1>
+    </div>
+  );
+}
+
+export default App;
