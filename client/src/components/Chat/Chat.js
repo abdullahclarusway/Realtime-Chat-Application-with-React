fb98a8aa521a728f98ac31ef6bbaedb385940d8b
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-01-06 23:28:52 +0100

    Refactoring chat component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
-
+import InfoBar from "../InfoBar/InfoBar";
 import TextContainer from "../TextContainer/TextContainer";
 import Messages from "../Messages/Messages";
-import InfoBar from "../InfoBar/InfoBar";
+
 import Input from "../Input/Input";
 
 import "./Chat.css";
